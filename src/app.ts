352477926ccca1d33ce76ea383d25a9bf8ef26ce
masud2005 <masud20012005@gmail.com>
2025-09-15 19:19:27 +0600

    user created route added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { UserRoutes } from './modules/user/user.route';
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1/user", UserRoutes);
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from "express";
+import { User } from "./user.model";
+import httpStatus from "http-status-codes";
+
+const createUser = async (req: Request, res: Response) => {
+    try {
+        const { name, email } = req.body;
+
+        const user = await User.create({
+            name,
+            email
+        })
+
+        res.status(httpStatus.CREATED).json({
+            message: "User Created Successfully",
+            user
+        })
+    } catch (error: any) {
+        // console.log(error);
+        res.status(httpStatus.BAD_REQUEST).json({
+            message: `Something Went Wrong!! ${error.message}`,
+            error
+        })
+    }
+}
+
+export const UserControllers = {
+    createUser
+}
