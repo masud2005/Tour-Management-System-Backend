6e59a0829168f8636278e70ec97db1a61bd60e71
masud2005 <masud20012005@gmail.com>
2025-09-26 17:41:21 +0600

    part-3 complete

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import cors from 'cors';
 import express, { Request, Response } from 'express';
+import cookieParser from "cookie-parser";
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/NotFound';
 import { router } from './app/routes';
+import passport from 'passport';
+import expressSession from 'express-session';
+import { envVars } from './app/config/env';
+import './app/config/passport';
 const app = express();
 
+app.use(expressSession({
+    secret: envVars.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 
